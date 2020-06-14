03ca38b0b4a8bff303d950a885483ad830c39872
VaibhavGurap <gurapvaibhav@gmail.com>
2020-06-14 12:56:10 +0530

    demo

---
+++ nirmal-dham-delhi.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="style.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.4.1/js/bootstrap.min.js"></script>
+</head>
+<body style="background-image: linear-gradient(to bottom, #ffffff, #f6f6ff, #eaedff, #dce5ff, #cbdeff, #c3dbff, #bad9ff, #b0d6ff, #b0d6ff, #b0d6ff, #b0d6ff, #b0d6ff);">
+
+<div class="container-fluid big-screen">
+  <div class="row">
+    <div class="col-sm-6 " >
+	<br>
+		<img src="http://localhost/sahajayogapune/wp-content/uploads/2020/06/Backup_of_back-page-design-final.gif" class="place-image">
+    </div>
+    <div class="col-sm-6" >
+	  <br>
+      <center class="place-title">SHRI ADISHAKTI PEETH Nirmal <br>Dham, Chhawla, New Delhi</center>
+	  <span class="place-subtitle"><b>&nbsp;Need for a New Ashram:&nbsp;&nbsp;</b></span><br>
+	  <p class="place-text" align="justify">
+	  <br>75<sup>th</sup> birthday celebration in Delhi; Seeing the constantly increasing number of Sahaja Yogis, Shri Mataji called
+	  the then Delhi S. Yoga leader and some trustees of L.E.T. Delhi and instructed them to find out and purchase a piece of land big 
+	  enough to organise events on such a big scale. Her words are commands for all Her children. Immediately they started this Holy Mission.
+	  Ultimately Shri Mataji approved of the lank of present Nirmal Dham Ashram at Chhawla village of
+	  South East Delhi. She was very happy to see the photographs and video of plot of land and decided that five acres of land be purchased.
+	  By Her blessings the land was purchased and got registered in the end of year 1999. Two and a half acres adjoining plot was acquired and added to it later.
+	  <br><br>The Birthday Puja Seminar 200 was approaching, so the construction and development work was started on war footing. Shri Adishakiti guided Delhi
+	  Sahaja Yogis on every front.</p>
+    </div>
+  </div>
+  <p class="place-text-2" align="justify">
+  Thousands trucks of ash were used for ground levelling. A big permanent stage was constructed. A well decked attached Rest Room was built was Shri mataji to 
+  take rest before or after the programme.
+  </p>
+  <span class="place-subtitle"><b>&nbsp;&nbsp;Naming of the Ashram&nbsp;&nbsp;</b></span>
+  <p class="place-text-2" align="justify"><br>
+  When the construction started, S. Yogis asked Shri Mataji as to what name should they give to this Ashram. Shri Mataji replied, 'Ye hamara Dham hoga, is liye ise Nirmal
+  Dham namm de do.' On another occasion She said to some yogis that since now I am getting old, I shall also need some land.<br><br>
+  First colourful Sakar Birthday celebration at N. Dham were held in March 2020. Since then till 2004 N. Dham hosted all the Birth Day and some Marriage Anniversary
+  celebrations of Her Holiness.<br><br>
+  On December 04, 2010, while leaving for Genova, Shri Mataji paid Her last Sakar Visit to N. Dham. There were as many as 6000 sahajis present and waiting to have a 
+  glimpse of the Mother Divine. She stayed there for 45 minutes. Sliently sitting in a speciilay designed car-chiar, from the stage she kept looking at the newly constructed
+  hangar and other developments all around. Her face was radiating like a multifacted diamond placed in the sun, its beam enlightening the heart and brains of Her beloved children<br><br>
+  On February 23, 2011, after fulfilling Her mission of awakening millions of seekers, Shri Adishakiti resumed Her Virata Form. Once again to the luck of Delhi, N.C.R., whole of India
+  and the worldm Shri Mataji choose Nirmal Dham to be Her permanent abode where She will rest in Sakar and Nirakar (form and formless) forever and ever.<br><br>
+  For two days and nights Sahaja Yogis worked ceaselessly to prepare the sanctum sanctoruim where the Sakar form of the Divine Mother was to be laid to rest. More than
+  a lakh people paid their obeisance to Her S. Yogis performed Puja and Aarti and thus this Adishakiti Peetha was created.<br><br>
+  When Shri Sati burnt herself in yajna of Yaksha - Her father. In the state of anger Shri Shiva beheaded Yaksha Prajapti, lifted the charred body of Shri Sati and moved all over. 
+  It is said that limbs of Shri Sati fell at 52 places and 52 Sakti Peethas came into existence. How sacrosanct, powerful and boon giving our N. Dham Ashram
+  would be, where Sakshat Shri Adishakiti reigns in Her full form (Sakar and Nirakar)? It is for all the S. Yogis to realize by their own Chaitanya Chetna. To quote from Her
+  Guru puja speech:<br><br><b>
+  <center style="color:blue; font-size:18px;font-style: italic;">
+  "Today is the day I declare that I am the one who has to save the humanity. I declare that I am the one who is Adishakiti, who is the Mother of all
+  the Mothers, who is Primordial Mother, the Adishakiti............."
+  </center>
+  <center style="font-size:18px;"><br>
+	Nirmal Dham, as such, is not only the Adishakiti Peetha, it is 'Sampoorna Adishakiti Peetha'.
+  </center>
+  </b>
+  <br>
+  </p>
+</div>
+
+ <p class="place-text-2" align="center">
+  No sooner than the construction of the Sanctum Sanctorum was completed, L.E.T. Delhi took up the gigantic task of further developing N. Dham Ashram and<br>
+  constructing the present shrine. Before the next Param Chaitanya Puja, N. Dham wore a completely transformed colourful look with omnipresent fragrance of<br>
+  vibratios blended with love emanating from every part of the campus. From each direction - East, West, North, South from above and below Divine presence of<br>
+  Shri Ganeha could be felt and experienced. The very look of the sanctum sanctorum was now like the Temple of God. A magnificent chandelier of six feet height<br>
+  spans the ceiling. It decorates and enlightens the inner hall. A ling traditional brass 'Kalash' adds to the grandeur of the 35 feet high spire of the shrine.<br>
+  </p>
+  <p class="place-text-2" style="margin-left:2%;margin-right:2%" align="justify">
+  Twi heavy weight white marble carved ferocious but elegant looking lions guard the entrance gate of the sanctum sanctorum. The 50 feet circular inner dimension of the
+  shrine, studded with pure white Makrana marble, has three large glass doors. The central gate presents a direct view of Devi's altar. Besides seven glass windows span
+  the entire central domal structure of the shrine. In total there are ten openings to the sanctum sanctorum. This number has a special spiritual. All the latitude
+  and longitude got fixed automatically, even the direction of Head and Lotus feet of Shri Mataji. Actually one She is the planner and doer.  <br><br>
+  The shrine has provision for devotional inner an douter circumambulation (parikrama).<br><br>
+  A hangar has been raised in front of the netranc eof the shrine for those seekers who are keen to have proximity of the Divine Mother. It is big enough to seat 5/600 S.
+  Yogi/yoginis. In front of the shrine and the main stage of puja - pandal, beautiful lush green lawns have been developed for the comforts of the chlidren of Divine Mother.<br><br><br>
+  The process of developing more and more facilities continues unabated. The present state of developments at N. Dham is as under:<br>
+  a)&nbsp;&nbsp;&nbsp;&nbsp; Double storey hangar of 15000 sq. feet area each has been constructed to accommodate some 2000 S. Yogis at the times of Seminars/Events.<br>
+  b)&nbsp;&nbsp;&nbsp;&nbsp; A new neat and clean Toilat Block, enough for 100 people at a time, has been built adjoining the hangar.<br>
+  c)&nbsp;&nbsp;&nbsp;&nbsp; A huge kitchen, sufficient to cook for 20,000 people has been built.<br>
+  d)&nbsp;&nbsp;&nbsp;&nbsp; A beautiful canteen with tiled flooring and walls has been constructed. It serves homely meals, snacks, tea, cold drinks etc. to the visiting yogis.<br>
+  e)&nbsp;&nbsp;&nbsp;&nbsp; A beautiful grassy lawn has also been developed in front of the canteen.<br>
+  f)&nbsp;&nbsp;&nbsp;&nbsp; Six well furnished A.C. Rooms with attached baths, drinking water and tea facilities are made ready for visiting yogis. Online reservation could be done.<br>
+  g)&nbsp;&nbsp;&nbsp;&nbsp; Ladies and gents well furnished dormitories to accommodate 100 yogis (aporx.) have been prepared.<br>
+  h)&nbsp;&nbsp;&nbsp;&nbsp; Besides all this, rows of lush green shady trees, blooming rose plants in abundance and seasonal flowers make N. Dham a Silvan Retreat worthy of visting again and again.<br>
+  i)&nbsp;&nbsp;&nbsp;&nbsp; Restroom constructed for Shri Mataji has now been converted into a beautiful Museum displaying photographs, furniture, and other articles used by Shri Mataji. S. yogis sit before it and meditate with great reverence for the beloved Mother.<br>
+  j)&nbsp;&nbsp;&nbsp;&nbsp; On the left side of the Museum, in a well protected transparent glass enclosure, the Adishakiti Rath (well maintained Mercedes car used by Shri Mataji for many years) has reverentially been displayed.
+  </p>
+  <p class="place-text-2" align="right">
+  From 5.30 a.m. till 9.00 p.m. N. Dham remains open for all seekers and S. Yogis. The ocean of Divine Mother's love, &nbsp;<br>
+  compassion and vibrations keeps surging day and night, always waiting for Her beloved children to come in the arms&nbsp;<br>
+  <p class="place-text-2" style="margin-left:37.5%;margin-top:-0.6%;">of Shri Adishakiti.</p>
+  </p>
+  <p style="font-size:25px;margin-right:1%" align="right">
+  Jai Shree Mataji
+  </p>
+  <div class="footer-strip">
+  <img src="http://localhost/sahajayogapune/wp-content/uploads/2020/06/footer-logo.png" style="margin-left:5%;">
+  </div>
+</body>
+</html>
